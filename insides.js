6150fc27f74f59b1b9ea09c4092a56cb0b379da6
VSHellkite <92489558+VSHellkite@users.noreply.github.com>
2023-05-22 00:28:36 +0100

    first insides

---
+++ insides.js
@@ -0,0 +1,18 @@
+class unit{
+    constructor(name, tag, system, value, currvalue){
+        this.name = name;
+        this.tag = tag;
+        this.system = system;
+        this.value = value;
+        this.currvalue = currvalue;
+        
+    }
+}
+let inch = new unit("inch","in.", 1, 1, 2.5);
+
+let cm = new unit("centimeter","cm.", 2,1,0.3937);
+
+    convert(){
+var result = i1 * unit.currvalue;
+}
+convert.result = i1 * inch.currvalue;
